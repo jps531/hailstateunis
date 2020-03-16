807b0ca9918c876cef7d062ffc313480f91025fb
psulliva <paul.sullivan@adtran.com>
2020-03-15 22:10:17 -0500

    inital commit

---
+++ frontEnd/tableTest.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Football Uniform History</title>
+    <?php include "../included/head.php"; ?>
+    <link rel="stylesheet" href="scss/tableTest.scss">
+    <?php include "../included/navbar.php"; ?>
+  </head>
+
+  <body>
+    <?php include "../included/footballTable.php"; ?>
+  </body>
+</html>
---
+++ included/navbar.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="../frontEnd/scss/navbar.scss">
+
+<nav class="navbar navbar-dark navbar-expand-md">
+  <a class="navbar-brand" href="testTest.php.tmp.html">
+    <img src="https://i.imgur.com/5Kqz72q.png" width="45" alt="" class="d-inline-block align-middle mr-2">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbarMd">
+    <i class="fas fa-bars"></i>
+  </button>
+  <div class="navbar-collapse collapse justify-content-end" id="collapsingNavbarMd">
+    <ul class="nav navbar-nav text-center">
+      <li class="nav-item">
+        <a class="nav-link" href="testTest.php.tmp.html">Home</a>
+      </li>
+    </ul>
+  </div>
+</nav>
