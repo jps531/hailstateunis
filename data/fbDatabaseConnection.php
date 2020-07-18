42005f35350981320850b6e95834b8934324e9ea
psulliva <paul.sullivan@adtran.com>
2020-07-17 20:12:36 -0500

    db update

--- data/fbDatabaseConnection.php
+++ data/fbDatabaseConnection.php
@@ -1,8 +1,8 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "ls-ed52acd835d3b04e8df8a962fff8a5e1f827d6c5.camxrlw6o2ww.us-east-1.rds.amazonaws.com";
+$username = "dbmasteruser";
+$password = "S2K`5)k{s:66~k#Zt=7Ym}SQ#z%!)s]g";
 $dbName = "footballTableData";
 
 // Create connection
