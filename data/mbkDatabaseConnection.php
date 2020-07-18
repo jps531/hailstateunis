a7e99d593d3093ec8b292440dffc9540f4e79514
psulliva <paul.sullivan@adtran.com>
2020-07-17 22:29:03 -0500

    db update pt.2

--- data/mbkDatabaseConnection.php
+++ data/mbkDatabaseConnection.php
@@ -3,7 +3,7 @@
 $servername = "ls-ed52acd835d3b04e8df8a962fff8a5e1f827d6c5.camxrlw6o2ww.us-east-1.rds.amazonaws.com";
 $username = "dbmasteruser";
 $password = "S2K`5)k{s:66~k#Zt=7Ym}SQ#z%!)s]g";
-$dbName = "mbkTableData";
+$dbName = "mbktabledata";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbName);
