715dc50758537429b8a2e84c5ee921d1f282e49d
psulliva <paul.sullivan@adtran.com>
2020-07-20 22:23:02 -0500

    fix blue glow and test email

---
+++ emailtest.php
@@ -0,0 +1,17 @@
+<?PHP
+$sender = 'hailstateunis@gmail.com';
+$recipient = 'hailstateunis@gmail.com';
+
+$subject = "php mail test";
+$message = "php test message";
+$headers = 'From:' . $sender;
+
+if (mail($recipient, $subject, $message, $headers))
+{
+    echo "Message accepted";
+}
+else
+{
+    echo "Error: Message not accepted";
+}
+?>
