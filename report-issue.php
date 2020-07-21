7b61f42be1f5b6d912f1b532ef62ac6b14c9544e
psulliva <paul.sullivan@adtran.com>
2020-07-21 13:22:50 -0500

    report issue fixed

--- report-issue.php
+++ report-issue.php
@@ -1,12 +1,17 @@
 <?php
 
-// Define variables and set to empty values
-$page = $category = $message = "";
-$pageError = $categoryError = $messageError = "";
-$errorTotal = 0;
-
 // Get previous URL
-$prevURL = $_SERVER['HTTP_REFERER'];
+if(isset($_SERVER['HTTP_REFERER'])) {
+  $prevURL = $_SERVER['HTTP_REFERER']; 
+}
+
+// If no previous URL, redirect to welcome
+else{
+  ob_start();
+  header('Location: welcome.php');
+  ob_end_flush();
+  die();
+}
 
 // Sanatize URL after failed sumbit
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -31,60 +36,21 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 // Set category default and redirect URL
 if (strpos($prevURL, 'football') !== false) {
   $categoryDefault = "Football";
-  $redirect = $prevURL;
 }
 else if (strpos($prevURL, 'baseball') !== false) {
   $categoryDefault = "Baseball";
-  $redirect = $prevURL;
 }
 else if (strpos($prevURL, 'mbasketball') !== false) {
   $categoryDefault = "Men's Basketball";
-  $redirect = $prevURL;
 }
 else if (strpos($prevURL, 'wbasketball') !== false) {
   $categoryDefault = "Women's Basketball";
-  $redirect = $prevURL;
 }
 else if (strpos($prevURL, 'welcome') !== false) {
   $categoryDefault = "Welcome";
-  $redirect = $prevURL;
 }
 else{
   $categoryDefault = "";
-  $redirect = "welcome.php";
-}
-
-// Action on submit
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-
-  // Get variable values
-  $page = $_POST["page"];
-  $category = $_POST["category"];
-  $topic = $page . " Issue Report: " . $category;
-  $message = $_POST["message"];
-
-  // Check for errors
-  if (!$page) {
-    $pageError = "EMPTY";
-    $errorTotal += 1;
-  }
-  if (!$category) {
-    $categoryError = "EMPTY";
-    $errorTotal += 1;
-  }
-  if (!$message) {
-    $messageError = "EMPTY";
-    $errorTotal += 1;
-  }
-
-  /* If no errors, send email and redirect URL
-  if ($errorTotal == 0) {
-    mail("hailstateunis@gmail.com", $topic, $message);
-    ob_start();
-    header('Location: ' . $redirect);
-    ob_end_flush();
-    die();
-   }*/
 }
 ?>
 
@@ -117,14 +83,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
             <!-- Page Select -->
             <div class="form-group">
-              <?php
-                if ($pageError === "EMPTY") {
-                  echo "<label for='page' class='label' style='color: #b52f2f;'>On which page is the issue occuring?&nbsp;<i><small>This field is required.</small></i></label>";
-                }
-                else{
-                  echo "<label for='page' class='label'>On which page is the issue occuring?</label>";
-                }
-              ?>
+              <label for='page' class='label'>On which page is the issue occuring?</label>
               <select class="selectpicker issue" name="page" id="page" title="Select Page">
                 <option value="Welcome" <?php echo (isset($_POST['page']) && $_POST['page'] === 'Welcome') ? 'selected' : ''; ?>>Welcome</option>
                 <option value="Football" <?php echo (isset($_POST['page']) && $_POST['page'] === 'Football') ? 'selected' : ''; ?>>Football</option>
@@ -136,14 +95,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
             <!-- Category Select -->
             <div class="form-group">
-              <?php
-                if ($categoryError === "EMPTY") {
-                  echo "<label for='category' class='label' style='color: #b52f2f;'>What type of issue are you reporting?&nbsp;<i><small>This field is required.</small></i></label>";
-                }
-                else{
-                  echo "<label for='category' class='label'>What type of issue are you reporting?</label>";
-                }
-              ?>
+              <label for='category' class='label'>What type of issue are you reporting?</label>
               <select class="selectpicker issue" name="category" id="category" title="Select Category">
                 <option value="Website Issue/Bug" <?php echo (isset($_POST['category']) && $_POST['category'] === 'Website Issue/Bug') ? 'selected' : ''; ?>>Website Issue/Bug</option>
                 <option value="Incorrect/Missing Table Data" <?php echo (isset($_POST['category']) && $_POST['category'] === 'Incorrect/Missing Table Data') ? 'selected' : ''; ?>>Incorrect/Missing Table Data</option>
@@ -153,21 +105,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
             <!-- Issue Message -->
             <div class="form-group">
-              <?php
-                if ($messageError === "EMPTY") {
-                  echo "<label for='message' class='label' style='color: #b52f2f;'>Describe the issue:&nbsp;<i><small>This field is required.</small></i></label>";
-                }
-                else{
-                  echo "<label for='message' class='label'>Describe the issue:</label>";
-                }
-              ?>
+              <label for='message' class='label'>Describe the issue:</label>
               <textarea name="message" id="message" class="form-control" placeholder="Issue"><?php echo isset($_POST['message']) 
               ? htmlspecialchars($_POST['message'], ENT_QUOTES) : ''; ?></textarea>
             </div>
 
             <!-- Submit Button -->
             <div class="text-center">
-              <button type="submit" class="btn submitBtn">Submit</button>
+              <button type="submit" class="btn submitBtn" disabled="disabled" id="reportIssueSubmit">Submit</button>
             </div>
           </form>
         </div>
@@ -176,11 +121,35 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
   </body>
 </html>
 
-<!-- Script to auto load page select -->
+<!-- Script -->
 <script>
+
+  // Auto load page select
   var categoryDefault = "<?php echo $categoryDefault; ?>";
   var categories = ["Welcome","Football","Baseball","Men's Basketball","Women's Basketball"];
   if(jQuery.inArray(categoryDefault, categories) !== -1){
     $('#page').selectpicker('val', categoryDefault);
+  }
+
+  // Enable / disable submit button
+  $('#page, #category, #message').bind('keyup', function() {
+    if(allFilled()){
+      $('#reportIssueSubmit').removeAttr('disabled');
+    }
+    else{
+      $('#reportIssueSubmit').attr('disabled', 'disabled');
+    }
+  });
+
+  // Function that determines whether all form elements are filled
+  function allFilled() {
+    var filled = true;
+    $('body select').each(function() {
+        if($(this).val() == '') filled = false;
+    });
+    $('body textarea').each(function() {
+        if($(this).val() == '') filled = false;
+    });
+    return filled;
   }
 </script>
