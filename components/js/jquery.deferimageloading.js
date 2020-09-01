52095d9ed3d32754eb66e7c6b2d394bad3d3efe0
psulliva <paul.sullivan@adtran.com>
2020-08-31 19:51:26 -0500

    football fix part 9: image loading tweaks- last one of the day

--- components/js/jquery.deferimageloading.js
+++ components/js/jquery.deferimageloading.js
@@ -8,24 +8,26 @@
 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+ * NOTE: -Paul Sullivan- 08-31-20: Code is heavily modified, but I'm leaving the copyright notice in as there are still some elements remaining.
  */
 
  (function($) {
    $.fn.deferImageLoading = function(callback) {
-     var group = this;
-     var count = this.length;
-
+    var group = this;
+    var count = this.length;
      return this.each(function() {
        var $img = $(this);
        $(document).ready(function() {
-         $img.attr('src', $img.data('src')).on('DOMContentLoaded', function() {
-           count--;
-           if (!count && $.isFunction(callback)) {
-             callback.call(group);
-           }
-         });
+        var downloadingImage = new Image();
+        downloadingImage.onload = function(){
+          $img.attr('src', this.src);
+          count--;
+          if (!count && $.isFunction(callback)) {
+            callback.call(group);
+          }
+        };
+        downloadingImage.src = $img.data('src');
        });
      });
-
    };
  }(jQuery));
