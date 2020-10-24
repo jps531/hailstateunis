64152feff9c27fa052f1ec00964c7eb46e534bc7
psulliva <paul.sullivan@adtran.com>
2020-10-23 21:02:11 -0500

    homecoming

--- components/taylorsvilleTable/taylorsvilleTable.php
+++ components/taylorsvilleTable/taylorsvilleTable.php
@@ -70,7 +70,7 @@
       $headcoach = $row["head_coach"];
       $postseason = $row["location"];
 
-      $homecomingArray = array("599","589","572","559","546","531","518","501","487");
+      $homecomingArray = array("617","599","589","572","559","546","531","518","501","487");
       if (in_array($row["gameID"], $homecomingArray)){
         $homecoming = " homecoming";
       }
