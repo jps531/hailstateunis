8a11cf8bf78cda332831fd7cfe3bc609817c6f8a
Paul Sullivan <43391133+jps531@users.noreply.github.com>
2020-11-15 20:27:59 -0600

    Delete duplicate file

--- components/southernmissTable/filters/searchSouthernMissTable.php
+++
@@ -1,3 +0,0 @@
-<!-- Search Field -->
-<input class="form-control" id="usmSearch" type="text" placeholder="Search.." autocomplete="off">
-<button class="btn clear" id="usmSearchClear"><i class="fas fa-times-circle"></i></button>
